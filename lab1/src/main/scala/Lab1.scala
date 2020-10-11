dcf650e56f37c93b799467d81b07df406e32ea26
michals96 <michal.stefaniuk9@gmail.com>
2020-10-12 00:54:53 +0200

    Call third function

--- lab1/src/main/scala/Lab1.scala
+++ lab1/src/main/scala/Lab1.scala
@@ -10,21 +10,34 @@ object Lab1 {
     // ************ TASK 1 ************************** //
     // Input two numbers (start, end) and print a list
     // of integers that from given distance.
-    val first = if (args.length == 2) args(0).toInt else 0
-    val last = if (args.length == 2) args(1).toInt else args(0).toInt
-    val list = List.range(first, last);
+    // val first = if (args.length == 2) args(0).toInt else 0
+    // val last = if (args.length == 2) args(1).toInt else args(0).toInt
+    // val list = List.range(first, last);
 
-    list.foreach(number => print(s"$number"))
+    // list.foreach(number => print(s"$number"))
   }
 
   def thirdTask(): Unit ={
+    val inventory = List(
+      ("rose while star", 12),
+      ("sunflower",        3),
+      ("orchid",           7),
+      ("carnation red",   25),
+      ("carnation white",  30)
+    )
 
+    printTuplelist(inventory)
   }
-  
+
+  def printTuplelist(value: List[(String, Int)]): Unit ={
+    value.foreach(item => println(f"${item._1}%20s: ${item._2}"))
+  }
+
   def main(args: Array[String]): Unit = {
 
     firstTask();
     secondTask();
+    thirdTask();
 
   }
 }
