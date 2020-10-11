621282946bf467eef2af8ee896841bd3265cd387
michals96 <michal.stefaniuk9@gmail.com>
2020-10-11 19:22:27 +0200

    Lab1 task2

--- lab1/src/main/scala/Hello.scala
+++ lab1/src/main/scala/Hello.scala
@@ -1,5 +1,16 @@
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+
+    print("Hello world!")
+
+
+    // ************ TASK 1 ************************** //
+    // Input two numbers (start, end) and print a list
+    // of integers that from given distance.
+    val first = if (args.length == 2) args(0).toInt else 0
+    val last = if (args.length == 2) args(1).toInt else args(0).toInt
+    val list = List.range(first, last);
+
+    list.foreach(number => print(s"$number"))
   }
 }
