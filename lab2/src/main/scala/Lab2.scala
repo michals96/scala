9bbef94584e0181c3c9094c29053e974507f297d
michals96 <michal.stefaniuk9@gmail.com>
2020-10-18 17:56:03 +0200

    Lab 2. Excercise no 2

--- lab2/src/main/scala/Lab2.scala
+++ lab2/src/main/scala/Lab2.scala
@@ -1,7 +1,14 @@
 import trigonometric.calculate
+import array.nicePrint
 
 object Lab2 {
   def main(args: Array[String]): Unit ={
+
+    val x = Array.ofDim[Int](4,4);
+    for (i <- 0 until 4; j <- 0 until 4) x(i)(j) = i+j;
+
     calculate(args(0), args(1));
+    nicePrint(x);
+
   }
 }
--- lab2/src/main/scala/array.scala
+++ lab2/src/main/scala/array.scala
@@ -1,3 +1,15 @@
 object array {
 
+  def niceRowPrint(array : Array[Int]): Unit ={
+    array foreach {
+      element => print(element);
+    }
+    println()
+  }
+
+  def nicePrint(array : Array[Array[Int]]): Unit ={
+    array foreach {
+      row => niceRowPrint(row)
+    }
+  }
 }
