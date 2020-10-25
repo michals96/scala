febd14bd6e487e55c1145bad846a7d05c0fc8c20
michals96 <michal.stefaniuk9@gmail.com>
2020-10-25 17:23:12 +0100

    Lab 3. Second task completed

--- lab3/src/main/scala/main.scala
+++ lab3/src/main/scala/main.scala
@@ -4,8 +4,18 @@ import firstTask.print1Darray
 import firstTask.sumColumn
 import firstTask.transposeArray
 import firstTask.mergeArrays
+import scala.math.{Pi}
 
 object main {
+
+  def pi = Pi
+  def pi(pi : Double) = pi*Pi
+  def repN(iterations : Int, x:Int => Int, arg: Int): Int ={
+    if(iterations != 1){
+      x(repN(iterations - 1, x, arg))
+    }
+    else x(arg)
+  }
   def main(args: Array[String]): Unit ={
     val x = Array.ofDim[Int](4,5);
     for(i <- 0 until 4; j<-0 until 5) x(i)(j) = i+j
@@ -18,5 +28,20 @@ object main {
     print2Darray(transposeArray(x))
     println()
     print2Darray(mergeArrays(x,x))
+
+    println("TASK2")
+
+    println(pi)
+    println(pi(pi))
+    println(pi(pi(pi)))
+    println(repN(5, (x:Int) => 2*x, 1))
   }
 }
+
+/*
+The goal is to write such function that the example code below compiles and gives expected results (on the right). Reminder, no external materials are allowed. 
+    println( pi )
+    println( pi(pi) ) //multiplication
+    println( pi(pi(pi)) )
+    println(repN(5,  (x: Int) => 2*x , 1 )) // repeat function application N times i.e. f(f(f(f(f(1))))), 1 the argument of the ﬁrst invocation
+ */
