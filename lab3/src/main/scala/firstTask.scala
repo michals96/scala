42086978ce1b72f0f0cbb351c3fccb9f3c626b05
michals96 <michal.stefaniuk9@gmail.com>
2020-10-25 16:51:05 +0100

    Lab 3. First task completed

--- lab3/src/main/scala/firstTask.scala
+++ lab3/src/main/scala/firstTask.scala
@@ -5,7 +5,7 @@ object firstTask {
   }
 
   def print1Darray(arr : Array[Int]): Unit ={
-    
+    arr foreach{element => print(element + " ")}
   }
 
   def sumRow(arr : Array[Array[Int]]): Array[Int] ={
@@ -20,7 +20,49 @@ object firstTask {
     sumRows
   }
 
-  def sumColumn(arr : Array[Array[Int]]): Unit ={
+  def sumColumn(arr : Array[Array[Int]]): Array[Int] ={
+    var sumCols = Array.ofDim[Int](5)
 
+    for(i <- 0 until 4){
+      for(j<-0 until 5){
+        sumCols.update(j, sumCols(j) + arr(i)(j))
+      }
+    }
+
+    sumCols
+  }
+
+  def transposeArray(arr : Array[Array[Int]]): Array[Array[Int]] ={
+    val arrT = Array.ofDim[Int](5,4)
+
+    for(i <- 0 until 4){
+      for(j<-0 until 5){
+        arrT(j)(i) = arr(i)(j)
+      }
+    }
+    arrT
+  }
+
+  def mergeArrays(firstArr: Array[Array[Int]], secondArr: Array[Array[Int]]): Array[Array[Int]] ={
+    val mergedArr = Array.ofDim[Int](4,5)
+    for(i <- 0 until 4){
+      for(j<-0 until 5){
+        mergedArr(i)(j) = firstArr(i)(j) + secondArr(i)(j)
+      }
+    }
+    mergedArr
   }
 }
+
+/*
+Goal is to write functions:
+ 1) to print this array in the following form:
+| 0 1 2 3 4 |
+| 1 2 3 4 5 |
+| 2 3 4 5 6 |
+| 3 4 5 6 7 |
+2) obtain two 1D arrays, one containing sum of elements in each row, and second containing sum of elements in each column
+3) to create an array that is transposed
+4) merge two arrays x and y of identical sizes (here 4x5) so that each element in the resulting array is larger of the corresponding element in x and y
+
+ */
--- lab3/src/main/scala/main.scala
+++ lab3/src/main/scala/main.scala
@@ -1,4 +1,9 @@
 import firstTask.print2Darray
+import firstTask.sumRow
+import firstTask.print1Darray
+import firstTask.sumColumn
+import firstTask.transposeArray
+import firstTask.mergeArrays
 
 object main {
   def main(args: Array[String]): Unit ={
@@ -6,5 +11,12 @@ object main {
     for(i <- 0 until 4; j<-0 until 5) x(i)(j) = i+j
 
     print2Darray(x)
+    print1Darray((sumRow(x)))
+    println()
+    print1Darray(sumColumn(x))
+    println()
+    print2Darray(transposeArray(x))
+    println()
+    print2Darray(mergeArrays(x,x))
   }
 }
