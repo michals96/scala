b20be32941e5d4098516cc1f364502d3789adba4
michals96 <michal.stefaniuk9@gmail.com>
2020-11-14 12:42:15 +0100

    Lab5 Hello world!

---
+++ lab5/build.sbt
@@ -0,0 +1,5 @@
+name := "lab5"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ lab5/src/main/scala/main.scala
@@ -0,0 +1,7 @@
+package main.scala
+
+object main {
+  def main(args: Array[String]): Unit ={
+    println("Hello world")
+  }
+}
