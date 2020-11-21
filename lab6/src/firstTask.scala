e389604571736cc100ca6cee11524512e65f7871
michals96 <michal.stefaniuk9@gmail.com>
2020-11-21 16:07:13 +0100

    Lab6. First task done

--- lab6/src/firstTask.scala
+++ lab6/src/firstTask.scala
@@ -1,6 +1,5 @@
 object firstTask {
   trait RichMoves {
-
     var posx: Double
     var posy: Double
     var rotation: Double
@@ -19,22 +18,34 @@ object firstTask {
     var posy = mypos._2
     var rotation = whereHeading
 
-    override def toString: String = s"$posx, $posy, $rotation"
+    override def toString: String = s"HORSE: $posx, $posy, $rotation"
+  }
+
+  class Car(var xpos: Double, var ypos: Double, var direction: Double) {
+    var posx = xpos
+    var posy = ypos
+    var rotation = direction
+
+    override def toString: String = s"CAR: $posx, $posy, $rotation"
   }
 
   def runFirstTask(): Unit ={
-    /*val car1 = new Car(0, 0, 0)
+    val car1 = new Car(0, 0, 0)
     println(car1)
 
     val betterCar = new Car(0, 0, 0) with RichMoves
     println(betterCar)
+
     betterCar.forward(5.0)
-    betterCAr.turnLeft
-    betterCar.turnBack
-    betterCar.forward(1)
-    println(betterCar)*/
+    betterCar.turnLeft(1.0)
+    betterCar.turnBack(1.0)
+    betterCar.forward(1.0)
+    println(betterCar)
 
     val runner = new Horse( (1,1), 270) with RichMoves
+    println(runner)
+
+    runner.turnLeft(1.0)
     println(runner)
   }
 }
